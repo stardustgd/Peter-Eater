54ded314664168b8687640b7b0317c209fe1a9c8
Julia Nguyen <juliatnguyen05@gmail.com>
2024-01-28 11:05:49 -0800

    updated ui for review page

--- components/ReviewComponents.tsx
+++ components/ReviewComponents.tsx
@@ -12,9 +12,60 @@ import {
 
 import { Button } from 'react-native'
 
+const styles = StyleSheet.create({
+    header: {
+        fontSize: 32,
+        backgroundColor: '#fff',
+        textAlign: 'center',
+    },
+    separator: {
+        marginVertical: 30,
+        height: 1,
+        width: '80%',
+      },
+      subhead: {
+        padding: 10,
+        fontSize: 15,
+        textAlign: 'center',
+        marginVertical: 8,
+        maxWidth: '100%',
+        maxHeight: '65%',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+      },
+
+      item: {
+        // backgroundColor: '#0064a4',
+        padding: 20,
+        fontSize: 22,
+        textAlign: 'center',
+        marginVertical: 8,
+        maxWidth: '100%',
+        maxHeight: '65%',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+}
+)
+
 const ReviewScreen = ({ navigation }: {navigation: any}) => {
     return (
-        <Text>Review Screen</Text>
+        <ScrollView>
+            <Text style={styles.header}>Review Screen</Text>
+            <Text style={styles.subhead}>Brandywine</Text>
+            <Text style={styles.item}>Banana</Text>
+            <Text style={styles.item}>Rating: 4</Text>
+            <Button 
+            title="Submit"
+            // onPress={() =>
+            //     navigation.navigate('Menu', { name: 'Jane' })
+            // }
+        />
+        </ScrollView>
     );
 };
 const ProfileScreen = ({ navigation, route }: {navigation: any, route: any}) => {
