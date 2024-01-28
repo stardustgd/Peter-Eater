48e9eabc03e0624de3302b638df6975a4a6e168d
Julia Nguyen <juliatnguyen05@gmail.com>
2024-01-27 16:36:28 -0800

    Created a home page

--- components/HomeComponent.tsx
+++ components/HomeComponent.tsx
@@ -1,5 +1,55 @@
 import { Text } from '@/components/Themed'
+import React, { useState } from 'react'
+import { Button, StyleSheet, SafeAreaView, SectionList, StatusBar, TouchableOpacity } from 'react-native'
 
-export default function HomeComponent() {
-  return <Text>yo</Text>
+import { Text, View } from './Themed'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: 500,
+    paddingTop: StatusBar.currentHeight,
+    marginHorizontal: 16,
+  },
+  item: {
+    backgroundColor: '#0064a4',
+    padding: 20,
+    marginVertical: 8,
+  },
+  header: {
+    fontSize: 32,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    color: '#ffffff',
+  },
+  itemTouchable: {
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+})
+
+export default function MenuComponent({ navigation }) {
+  return (
+    <SafeAreaView style={styles.container}>
+      <SectionList
+        sections={data}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => (
+          <View>
+            <TouchableOpacity
+              onPress={() => console.log(`pressed ${item}`)}
+              style={styles.item}>
+              <Text style={styles.title}>{item}</Text>
+            </TouchableOpacity>
+          </View>
+        )
+        }
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.header}>{title}</Text>
+        )}
+      />
+    </SafeAreaView >
+  )
 }
